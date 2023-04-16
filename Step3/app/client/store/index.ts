e67cd8e5c22ccbb58b74ad1eb436ade0b824adf6
Deepanshu Arora <deepanshu.arora@deliveroo.com>
2023-04-16 20:21:50 +0530

    Adding support for redux dev tools extension

--- Step3/app/client/store/index.ts
+++ Step3/app/client/store/index.ts
@@ -1,6 +1,20 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import selectedValueReducer from '../reducer';
 
-const store = createStore(selectedValueReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  // eslint-disable-next-line no-underscore-dangle
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? // eslint-disable-next-line no-underscore-dangle
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
+const store = createStore(selectedValueReducer, enhancer);
 export default store;
